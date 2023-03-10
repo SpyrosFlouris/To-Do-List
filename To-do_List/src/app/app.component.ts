d2e41011dd35c1096585a15ab8e97d3c7c8db7f8
SpyrosFlouris <spyrosflouris2001@gmail.com>
2023-03-10 18:19:17 +0200

    fully functional add task, remove task and exit buttons

--- To-do_List/src/app/app.component.ts
+++ To-do_List/src/app/app.component.ts
@@ -9,8 +9,16 @@ export class AppComponent{
   title = 'To-do_List';
   isActive = false;
   Add = false;
-  placeholderText = "Enter text here";
-  inputDisabled = true;
+  placeholderText1 = "Enter text here";
+  placeholderText2 = "Enter text here";
+  placeholderText3 = "Enter text here";
+  placeholderText4 = "Enter text here";
+  placeholderText5 = "Enter text here";
+  placeholderText6 = "Enter text here";
+  placeholderText7 = "Enter text here";
+  placeholderText8 = "Enter text here";
+  placeholderText9 = "Enter text here";
+  placeholderText10 = "Enter text here";
 
   ngOnInit(){
 
@@ -21,9 +29,88 @@ export class AppComponent{
   }
 
   getUserInput(){
+    const numberInput = prompt("Please enter the number of the task:");
     const userInput = prompt("Please enter your input:");
-    if (userInput !== null) {
-      this.placeholderText = userInput;
-      this.inputDisabled = false;
+    if (userInput !== null && numberInput !== null) {
+      if (numberInput == "1") {
+        this.placeholderText1 = userInput;
+      }
+      else if (numberInput == "2") {
+        this.placeholderText2 = userInput;
+      }
+      else if (numberInput == "3") {
+        this.placeholderText3 = userInput;
+      }
+      else if (numberInput == "4") {
+        this.placeholderText4 = userInput;
+      }
+      else if (numberInput == "5") {
+        this.placeholderText5 = userInput;
+      }
+      else if (numberInput == "6") {
+        this.placeholderText6 = userInput;
+      }
+      else if (numberInput == "7") {
+        this.placeholderText7 = userInput;
+      }
+      else if (numberInput == "8") {
+        this.placeholderText8 = userInput;
+      }
+      else if (numberInput == "9") {
+        this.placeholderText9 = userInput;
+      }
+      else if (numberInput == "10") {
+        this.placeholderText10 = userInput;
+      }
+      else {
+        alert("Wrong input!")
+      }
   }
-}}
+}
+RemoveTask(){
+  const numberInput = prompt("Please enter the number of the task:");
+  if (numberInput !== null) {
+    if (numberInput == "1") {
+      this.placeholderText1 = "Enter text here";
+    }
+    else if (numberInput == "2") {
+      this.placeholderText2 = "Enter text here";
+    }
+    else if (numberInput == "3") {
+      this.placeholderText3 = "Enter text here";
+    }
+    else if (numberInput == "4") {
+      this.placeholderText4 = "Enter text here";
+    }
+    else if (numberInput == "5") {
+      this.placeholderText5 = "Enter text here";
+    }
+    else if (numberInput == "6") {
+      this.placeholderText6 = "Enter text here";
+    }
+    else if (numberInput == "7") {
+      this.placeholderText7 = "Enter text here";
+    }
+    else if (numberInput == "8") {
+      this.placeholderText8 = "Enter text here";
+    }
+    else if (numberInput == "9") {
+      this.placeholderText9 = "Enter text here";
+    }
+    else if (numberInput == "10") {
+      this.placeholderText10 = "Enter text here";
+    }
+    else {
+      alert("Wrong input!")
+    }
+  }
+}
+
+CloseWindow(){
+  window.close();
+}
+
+}
+
+
+
